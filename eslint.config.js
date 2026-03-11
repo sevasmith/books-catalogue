0ead927122b9c10e27dd8509c4767adbebda53ef
Seva Kavalenka <seva.kavalenka@gmail.com>
2026-03-11 12:17:14 +0100

    init: create a project with Vite, ESLint and Prettier

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default defineConfig([
+  {
+    ignores: ['node_modules/', 'dist/'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js, prettier: prettierPlugin },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+]);
